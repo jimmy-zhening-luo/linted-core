3df1ede1f25dc7067c0c8fa296a4a460719477ef
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-20 17:28:22 -0700

    decompose file default/include; also includes is partial

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,8 @@ export type { default as IConfig } from "./interface/IConfig.js";
 export default function (
   plugins: Plugins,
   parsers: Parsers,
-  files: Record<
-    | "defaults"
-    | "includes"
-    ,
-    Record<Scopes, string[]>
-  >,
+  defaults: Record<Scopes, string[]>,
+  includes: Partial<Record<Scopes, string[]>>,
   rulesets: { [S in Scopes]: Ruleset<S> },
 ): IConfig[] {
   try {
@@ -30,16 +26,16 @@ export default function (
       js: new options
         .js(
           plugins.js,
-          ...files.defaults.js,
-          ...files.includes.js,
+          ...defaults.js,
+          ...includes.js ?? [],
         )
         .body,
       ts: new options
         .ts(
           plugins.ts,
           parsers.ts,
-          ...files.defaults.ts,
-          ...files.includes.ts,
+          ...defaults.ts,
+          ...includes.ts ?? [],
         )
         .body,
       svelte: new options
@@ -47,40 +43,40 @@ export default function (
           plugins.svelte,
           parsers.svelte,
           parsers.ts,
-          ...files.defaults.svelte,
-          ...files.includes.svelte,
+          ...defaults.svelte,
+          ...includes.svelte ?? [],
         )
         .body,
       html: new options
         .html(
           plugins.html,
           parsers.html,
-          ...files.defaults.html,
-          ...files.includes.html,
+          ...defaults.html,
+          ...includes.html ?? [],
         )
         .body,
       json: new options
         .json(
           plugins.json,
           parsers.json,
-          ...files.defaults.json,
-          ...files.includes.json,
+          ...defaults.json,
+          ...includes.json ?? [],
         )
         .body,
       jsonc: new options
         .jsonc(
           plugins.jsonc,
           parsers.jsonc,
-          ...files.defaults.jsonc,
-          ...files.includes.jsonc,
+          ...defaults.jsonc,
+          ...includes.jsonc ?? [],
         )
         .body,
       yml: new options
         .yml(
           plugins.yml,
           parsers.yml,
-          ...files.defaults.yml,
-          ...files.includes.yml,
+          ...defaults.yml,
+          ...includes.yml ?? [],
         )
         .body,
     };
