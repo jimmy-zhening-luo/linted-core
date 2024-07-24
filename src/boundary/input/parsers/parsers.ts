d8a7487ba102264b7519b7d3933eb64425a44762
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-23 20:45:44 -0700

    reaaaadonly

--- src/boundary/input/parsers/parsers.ts
+++ src/boundary/input/parsers/parsers.ts
@@ -2,7 +2,7 @@ import type { Scope } from "../input.js";
 import type { Options } from "../../../factory/Factory.js";
 
 type Parsers = {
-  [S in Scope]: ConstructorParameters<typeof Options[S]>[2] extends readonly [unknown, ...unknown[]]
+  readonly [S in Scope]: ConstructorParameters<typeof Options[S]>[2] extends readonly [unknown, ...unknown[]]
     ? unknown
     : null
 };
