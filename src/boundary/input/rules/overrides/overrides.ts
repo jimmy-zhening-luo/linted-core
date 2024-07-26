5d971f8e9df285cfa40ad41e9e849be842f697e2
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-26 13:50:23 -0700

    partial record?

--- src/boundary/input/rules/overrides/overrides.ts
+++ src/boundary/input/rules/overrides/overrides.ts
@@ -1,7 +1,7 @@
 import type { Scope } from "../../../../scopes/Scopes.js";
 import type * as Overrides from "../preset/preset.js";
 
-type Overrides = Readonly<Particord<Scope, Overrides.Preset.Entry.Object>>;
+type Overrides = Readonly<Partial<Record<Scope, Overrides.Preset.Entry.Object>>>;
 
 export type {
   Overrides as default,
