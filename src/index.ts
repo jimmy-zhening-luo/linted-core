5387d7811530b3baee1bc63da8a4ee3410f76c30
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-08-23 20:28:50 -0700

    get rid of parser bag

--- src/index.ts
+++ src/index.ts
@@ -12,80 +12,69 @@ export default function (
   try {
     const F = new Files(files),
     R = new Rulesets(rules),
-    Pa: { [S in Scope]: ConstructorParameters<typeof Options[S]>[3] } = {
-      js: [] as const,
-      ts: [parsers.ts] as const,
-      svelte: [parsers.svelte, parsers.ts] as const,
-      mocha: [parsers.ts] as const,
-      html: [parsers.html] as const,
-      json: [parsers.jsonc] as const,
-      jsonc: [parsers.jsonc] as const,
-      yml: [parsers.yml] as const,
-      md: [parsers.md] as const,
-    } as const,
     options: { [S in Scope]: InstanceType<typeof Options[S]>["configs"] } = {
       js: new Options
         .js(
           F.files("js"),
           R.ruleset("js"),
           { "@stylistic": plugins["@stylistic"] },
-          Pa.js,
+          [],
         ).configs,
       ts: new Options
         .ts(
           F.files("ts"),
           R.ruleset("ts"),
           { "@stylistic": plugins["@stylistic"], "@typescript-eslint": plugins["@typescript-eslint"] },
-          Pa.ts,
+          [parsers.ts],
         ).configs,
       svelte: new Options
         .svelte(
           F.files("svelte"),
           R.ruleset("svelte"),
           { "@stylistic": plugins["@stylistic"], "@typescript-eslint": plugins["@typescript-eslint"], svelte: plugins.svelte },
-          Pa.svelte,
+          [parsers.svelte, parsers.ts],
         ).configs,
       mocha: new Options
         .mocha(
           F.files("mocha"),
           R.ruleset("mocha"),
           { "@stylistic": plugins["@stylistic"], "@typescript-eslint": plugins["@typescript-eslint"], mocha: plugins.mocha },
-          Pa.mocha,
+          [parsers.ts],
         ).configs,
       html: new Options
         .html(
           F.files("html"),
           R.ruleset("html"),
           { "@html-eslint": plugins["@html-eslint"] },
-          Pa.html,
+          [parsers.html],
         ).configs,
       json: new Options
         .json(
           F.files("json"),
           R.ruleset("json"),
           { jsonc: plugins.jsonc },
-          Pa.json,
+          [parsers.jsonc],
         ).configs,
       jsonc: new Options
         .jsonc(
           F.files("jsonc"),
           R.ruleset("jsonc"),
           { jsonc: plugins.jsonc },
-          Pa.jsonc,
+          [parsers.jsonc],
         ).configs,
       yml: new Options
         .yml(
           F.files("yml"),
           R.ruleset("yml"),
           { yml: plugins.yml },
-          Pa.yml,
+          [parsers.yml],
         ).configs,
       md: new Options
         .md(
           F.files("md"),
           R.ruleset("md"),
           { markdownlint: plugins.markdownlint },
-          Pa.md,
+          [parsers.md],
         ).configs,
     };
 
