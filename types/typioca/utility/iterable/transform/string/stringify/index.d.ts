061053283e98ac030c412f1f56ce76d7de1af5d2
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-09-17 22:30:58 -0700

    - errant test case in typioca

--- types/typioca/utility/iterable/transform/string/stringify/index.d.ts
+++ types/typioca/utility/iterable/transform/string/stringify/index.d.ts
@@ -11,17 +11,3 @@ declare type Stringify<C> = C extends abstract new (...args: infer A) => infer O
       : C extends { toString: () => infer S extends string }
         ? S
         : never;
-
-declare namespace Stringify {
-  export type T = Stringify<CharString<"OK-Instance">>;
-  export type T0 = Stringify<CharStringCtor>;
-  export type T1 = Stringify<charstringfunc<"OK-Function">>;
-
-  type CharStringCtor = abstract new () => CharString<"OK-Constructor">;
-
-  interface CharString<Validator extends string> {
-    readonly string: valid<stringful, [Validator, "string"]>;
-  }
-
-  type charstringfunc<Validator extends string> = () => valid<stringful, [Validator, "string"]>;
-}
