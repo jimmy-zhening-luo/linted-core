7932f94c1ac881f13170ca0f503d17264ce84da3
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2024-10-25 00:12:48 -0700

    begin factory wrapper over files/rules

--- src/factory/index.ts
+++ src/factory/index.ts
@@ -1,7 +1,26 @@
 export type { Scope, Input, Output } from "..";
 
-import Options from "./options";
+import type { Input } from "..";
 import { Files } from "./files";
 import { Rulesets } from "./rulesets";
 
-export { Options, Files, Rulesets };
+export Options from "./options";
+export class Factory {
+  public readonly files: Files;
+  public readonly rulesets: Rulesets;
+
+  constructor(
+    files: Input["files"],
+    rules: Input["rules"],
+  ) {
+    this.files = new Files(files);
+    this.rulesets = new Rulesets(rules);
+  }
+
+  public produce(scope: string) {
+    return [
+      this.files.files(scope),
+      this.rulesets.ruleset(scope),
+    ] as const;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -7,72 +7,67 @@ export type { Input, Output };
 
 import { scopes } from "./scopes";
 import {
-  Files,
-  Rulesets,
+  Factory,
   Options,
 } from "./factory";
 
 export default function (input: Input): Output {
   try {
-    const { parsers, plugins } = input,
-    files = new Files(input.files),
-    rulesets = new Rulesets(input.rules),
+    const {
+      parsers,
+      plugins,
+      files,
+      rules,
+    } = input,
+    factory = new Factory(files, rules),
     options: { [S in typeof scopes[number]]: InstanceType<typeof Options[S]>["configs"] } = {
       js: new Options
         .js(
           { "@stylistic": plugins["@stylistic"] },
           [],
-          files.files("js"),
-          rulesets.ruleset("js"),
+          ...factory.produce("js"),
         ).configs,
       ts: new Options
         .ts(
           { "@stylistic": plugins["@stylistic"], "@typescript-eslint": plugins["@typescript-eslint"] },
           [parsers.ts],
-          files.files("ts"),
-          rulesets.ruleset("ts"),
+          ...factory.produce("ts"),
         ).configs,
       svelte: new Options
         .svelte(
           { "@stylistic": plugins["@stylistic"], "@typescript-eslint": plugins["@typescript-eslint"], svelte: plugins.svelte },
           [parsers.svelte, parsers.ts],
-          files.files("svelte"),
-          rulesets.ruleset("svelte"),
+          ...factory.produce("svelte"),
         ).configs,
       mocha: new Options
         .mocha(
           { "@stylistic": plugins["@stylistic"], "@typescript-eslint": plugins["@typescript-eslint"], mocha: plugins.mocha },
           [parsers.ts],
-          files.files("mocha"),
-          rulesets.ruleset("mocha"),
+          ...factory.produce("mocha"),
         ).configs,
       html: new Options
         .html(
           { "@html-eslint": plugins["@html-eslint"] },
           [parsers.html],
-          files.files("html"),
-          rulesets.ruleset("html"),
+          ...factory.produce("html"),
         ).configs,
       json: new Options
         .json(
           { jsonc: plugins.jsonc },
           [parsers.jsonc],
-          files.files("json"),
-          rulesets.ruleset("json"),
+          ...factory.produce("json"),
         ).configs,
       jsonc: new Options
         .jsonc(
           { jsonc: plugins.jsonc },
           [parsers.jsonc],
-          files.files("jsonc"),
-          rulesets.ruleset("jsonc"),
+          ...factory.produce("jsonc"),
         ).configs,
       yml: new Options
         .yml(
           { yml: plugins.yml },
           [parsers.yml],
-          files.files("yml"),
-          rulesets.ruleset("yml"),
+          ...factory.produce("yml"),
         ).configs,
     };
 
