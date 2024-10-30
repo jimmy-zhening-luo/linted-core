dc821d2e803641d215a1b235f3c4d881d82e904b
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-10-29 22:29:11 -0700

    test: scopes must check inclusion

--- src/scopes.spec.ts
+++ src/scopes.spec.ts
@@ -5,24 +5,31 @@ describe("Scopes", function () {
   describe("shape", function () {
     it("is a non-empty array", function () {
       expect(scopes)
-        .to.be.an("array")
-        .that.has.lengthOf.at.least(1);
+        .an("array")
+        .with.lengthOf.at.least(1);
     });
   });
   describe("order", function () {
     it("jsonc > json", function () {
+      expect(scopes)
+        .includes.members(["jsonc", "json"]);
       expect(scopes.indexOf("jsonc"))
         .greaterThan(scopes.indexOf("json"));
     });
     it("ts > js", function () {
+      expect(scopes)
+        .includes.members(["ts", "js"]);
       expect(scopes.indexOf("ts"))
         .greaterThan(scopes.indexOf("js"));
     });
     it("mocha > ts", function () {
+      expect(scopes)
+        .includes.members(["mocha", "ts"]);
       expect(scopes.indexOf("mocha"))
         .greaterThan(scopes.indexOf("ts"));
     });
     it("svelte > ts", function () {
+      expect(scopes).includes.members(["svelte", "ts"]);
       expect(scopes.indexOf("svelte"))
         .greaterThan(scopes.indexOf("ts"));
     });
