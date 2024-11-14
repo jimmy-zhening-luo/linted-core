729c959fb105ba946ed8fefc08d10b293cbf3079
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-11-14 00:01:16 -0800

    runoll only

--- src/interface/configs/globals/only/index.ts
+++ src/interface/configs/globals/only/index.ts
@@ -1,1 +1,4 @@
-export type Only<Base extends object, This extends object> = This & Readonly<Partial<Record<Exclude<keyof Base, keyof This>, never>>>;
+export type Only<
+  Base extends object,
+  This extends object,
+> = This & Readonly<Partial<Record<Exclude<keyof Base, keyof This>, never>>>;
