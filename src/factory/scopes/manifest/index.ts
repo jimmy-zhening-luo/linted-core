6c9f7cd608f1e6774af1de8cc22a1fafa0af551b
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2024-11-17 01:39:06 -0800

    -manifest parseropt

--- src/factory/scopes/manifest/index.ts
+++ src/factory/scopes/manifest/index.ts
@@ -1,10 +1,7 @@
 import type globals from "globals";
 import type { Parsers } from "../../../scopes";
 
-export abstract class ScopeManifest<
-  Parser extends Parsers | false = false,
-  ParserOptions extends object = object,
-> {
+export abstract class ScopeManifest<Parser extends Parsers | false = false> {
   public abstract readonly processor: { readonly processor: string } | Record<string, never>;
   public abstract readonly language: { readonly language: string } | Record<string, never>;
   public abstract readonly parserOptions: object & { readonly parser: null | Exclude<Parser, boolean> };
