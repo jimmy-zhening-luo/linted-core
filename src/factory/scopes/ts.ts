fd075e33f976ef76afc0f28a15d1ab5e20557844
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-11-24 16:36:45 -0800

    tslint parser options: update to v8.15 projectService
    - upgrade to latest project inference using projectService instead of project, optionally with tsconfigrootdir to set an absolute project root (disabled)
      - commented out for now
      - i believe it needs to be an absolute path
      - curious how it changes how ESLint from CLI or from VSCode interact with various files being linted especially in:
        - an IDE setting
        - from CLI in a subdir from where eslintconfig is, e.g. in directories without linted installed if eslint.config.js is not re-exported as it currently is
        - install @types/node and include in global types if eventually import.meta.dirname will be used to get the absolute path
        - need to see how import.meta.dirname works, will it get absolute path of the eslintconfig file where it is being invoked? i believe so, but needs further testing
    - allow global return
    - explicitly disable jsx (off by default anyway)
    - never parse jsdoc
    - +warnontsversion off
    - +docs

--- src/factory/scopes/ts.ts
+++ src/factory/scopes/ts.ts
@@ -6,8 +6,17 @@ export default class TsManifest extends ScopeManifest<"ts"> {
     globals: null,
   } as const;
   public readonly parserOptions = {
+    // DOC: https://typescript-eslint.io/packages/parser/#configuration
+    // DOC:(USE-CASE): https://typescript-eslint.io/getting-started/typed-linting/
     parser: null,
-    project: "tsconfig.json",
+    ecmaFeatures: {
+      jsx: false,
+      globalReturn: true,
+    },
+    jsDocParsingMode: "none" /* @default(project): "all" | @default: "none" | "type-info" */,
+    projectService: true /* DOC: https://typescript-eslint.io/packages/parser/#projectservice , DOC: https://typescript-eslint.io/troubleshooting/typed-linting/#project-service-issues */,
+    // tsconfigRootDir: import.meta.dirname /* DOC: https://typescript-eslint.io/packages/parser/#tsconfigrootdir ; see `DOC:(USE-CASE), I am inferring that an aboslute path is needed ; DOC:(Node.js: import.meta): https://nodejs.org/api/esm.html#importmetadirname */,
+    warnOnUnsupportedTypeScriptVersion: false,
     sourceType: "module",
     ecmaVersion: 2023,
   } as const;
