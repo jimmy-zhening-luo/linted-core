653654a2ba52e404b136faee8ca5463c710ff5ee
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2025-01-08 13:56:25 -0800

    +yaml parser option: defaultYAMLVersion (use default)

--- src/factory/scopes/yml.ts
+++ src/factory/scopes/yml.ts
@@ -5,7 +5,11 @@ export default class YmlManifest extends ScopeManifest<"yml"> {
     parser: "yml",
     globals: null,
   } as const;
-  public readonly parserOptions = { parser: null } as const;
+  public readonly parserOptions = {
+    /* DOC: https://github.com/ota-meshi/yaml-eslint-parser?tab=readme-ov-file#advanced-configuration */
+    parser: null,
+    defaultYAMLVersion: "1.2" /* @default: "1.2" | "1.1" */,
+  } as const;
   public readonly processor = {} as const;
   public readonly language = {} as const;
 }
