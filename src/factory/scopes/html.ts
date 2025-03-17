96101c39eb8afa221f6df8fd73580567cb500beb
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-03-17 14:29:56 -0700

    htmlint: skip linting frontmatter

--- src/factory/scopes/html.ts
+++ src/factory/scopes/html.ts
@@ -5,7 +5,10 @@ export default class HtmlManifest extends ScopeManifest<"html"> {
     parser: "html",
     globals: null,
   } as const;
-  public readonly parserOptions = { parser: null } as const;
+  public readonly parserOptions = {
+    parser: null,
+    frontmatter: true /* DOC: https://github.com/yeonjuan/html-eslint/issues/291#issuecomment-2726307803 */,
+  } as const;
   public readonly processor = {} as const;
   public readonly language = {} as const;
 }
