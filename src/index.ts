b8d7f5ffb54bb5bdd091667053dd6675feaed00e
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-05-24 20:20:54 -0700

    reduce type pollution down to minimum used set (only input for IDE hints surfaced at linted interface); slight linting -- proto still include for now

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,40 @@ import type { Input, Output } from "./interface";
 import { scopes, tree } from "./scopes";
 import { Factory } from "./factory";
 
-export type * from "./interface";
+export type * from "./interface/input";
+export type * from "./interface/proto";
 export type * from "./scopes";
-export default function ({
-  imports: { plugins, parsers },
-  defaults,
-  extensions,
-}: Input): Output {
+export default function (
+  {
+    imports: {
+      plugins,
+      parsers,
+    },
+    defaults,
+    extensions,
+  }: Input,
+) {
   try {
-    const factory = new Factory(tree, parsers, defaults, extensions);
+    const factory = new Factory(
+      tree,
+      parsers,
+      defaults,
+      extensions,
+    );
 
     return [
-      { name: "linted/*/plugins/", plugins } as const,
+      {
+        name: "linted/*/plugins/",
+        plugins,
+      },
       ...factory.globals,
       ...scopes.flatMap(scope => factory.scope(scope)),
-    ] as const;
+    ] satisfies Output satisfies unknown[] as unknown[];
   }
   catch (e) {
-    throw new Error(`Linted Core`, { cause: e });
+    throw new Error(
+      "linted-core: ",
+      { cause: e },
+    );
   }
 }
