9824803713c66b79f50bdc19609ee064a11d52d8
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-10 17:30:23 -0800

    Add rules property to GlobalExtension type

--- typings/extensions/global.d.ts
+++ typings/extensions/global.d.ts
@@ -1,6 +1,9 @@
+import type { Rules } from "../rules";
+
 export type GlobalExtension = {
   "*": {
     readonly ignores?: readonly string[];
     readonly override?: boolean;
+    readonly rules?: Rules;
   };
 };
